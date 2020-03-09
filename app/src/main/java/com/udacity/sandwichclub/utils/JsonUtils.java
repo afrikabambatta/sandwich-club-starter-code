d4ff984a257f2cd6997bf6c9004656854aa64035
Clement <ctintran@gmail.com>
2020-03-09 15:30:44 -0700

    Modify JsonUtils to parse the JSON Object and return a Sandwich object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,10 +2,17 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
 
+        // TODO: Is this the correct place to declare identifiers?
         /* All names are children of the "name" object. */
         final String NAME = "name";
 
@@ -25,7 +32,47 @@ public class JsonUtils {
         /* A string array of ingredients required to make the sandwich */
         final String INGREDIENTS = "ingredients";
 
-        return null;
+        /* The sandwich object we will populate with data from JSON Object */
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            /* JSONObject created using string array "sandwich_details" from string.xml */
+            JSONObject sandwichJson = new JSONObject(json);
+
+            /* Get JSON object that contains sandwich names */
+            JSONObject name = sandwichJson.getJSONObject(NAME);
+
+            /* Get the sandwich name */
+            sandwich.setMainName(name.getString(MAIN_NAME));
+
+            /* Parse the JSON array to get sandwich nicknames */
+            JSONArray akaArray = name.getJSONArray(NICKNAME);
+            ArrayList<String> akaList = new ArrayList<String>();
+            for (int i = 0; i < akaArray.length(); i++) {
+                akaList.add(akaArray.getString(i));
+            }
+            sandwich.setAlsoKnownAs(akaList);
+
+            /* Get sandwich origin */
+            sandwich.setPlaceOfOrigin(sandwichJson.getString(ORIGIN));
+
+            /* Get sandwich description */
+            sandwich.setDescription(sandwichJson.getString(DESCRIPTION));
+
+            /* Get sandwich image */
+            sandwich.setImage(sandwichJson.getString(IMAGE));
+
+            /* Retrieve JSONArray of ingredients and convert to ArrayList */
+            JSONArray ingredientsArray = sandwichJson.getJSONArray(INGREDIENTS);
+            ArrayList<String> ingredientsList = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredientsList.add(ingredientsArray.getString(i));
+            }
+            sandwich.setIngredients(ingredientsList);
+        } catch (JSONException e){ // TODO: Investigate if this is the correct exception
+            e.printStackTrace();
+        }
+        return sandwich;
 
     }
 }
